6cc0335a7857852abbdccdcf1a43ee9fcf762885
Aditya Prerepa <adiprerepa@gmail.com>
2021-02-28 11:29:11 -0800

    initial commit

---
+++ proto/codegen.sh
@@ -0,0 +1,1 @@
+../venv/bin/python3 -m grpc_tools.protoc -I. --python_out=../src --grpc_python_out=../src ./chunk.proto
---
+++ src/demo_client.py
@@ -0,0 +1,17 @@
+import os
+
+import lib
+
+
+client = lib.FileClient('localhost:8888')
+in_file_name = '/home/aditya/Downloads/5GiB.bin'
+
+
+def bench_gRPC_upload(benchmark):
+    benchmark(client.upload, in_file_name)
+    
+if __name__ == '__main__':
+
+    # demo for file uploading
+    client.upload(in_file_name)
+
